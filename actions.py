4a35ccb9ca027a0ed236c05930eea3a597adf432
SauravRoy50080 <131740964+SauravRoy50080@users.noreply.github.com>
2025-09-16 00:18:44 +0530

    actions.py

--- actions.py
+++ actions.py
@@ -1,1 +1,33 @@
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import sqlite3
+
+class ActionGetFee(Action):
+    def name(self) -> str:
+        return "action_get_fee"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: dict):
+        course = tracker.get_slot("course")
+
+        conn = sqlite3.connect("campus.db")
+        cur = conn.cursor()
+        cur.execute("SELECT fee FROM courses WHERE name=?", (course,))
+        result = cur.fetchone()
+        conn.close()
+
+        if result:
+            dispatcher.utter_message(text=f"The fee for {course} is {result[0]}")
+        else:
+            dispatcher.utter_message(text="Sorry, I don’t know that course.")
+        return []
+
+class ActionHandoff(Action):
+    def name(self) -> str:
+        return "action_handoff"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Transferring you to a human agent...")
+        return []
 
